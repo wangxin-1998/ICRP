ccde9a1494cd1e7d6119b6bab0092c1685797cce
Xu-SY <1010392329@qq.com>
2020-03-04 21:15:08 +0800

    reload

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from flask import Flask, render_template
-
+from flask import Flask,request,render_template
+import pymysql
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route('/info_browse')
+@app.route('/browseInfo')
 def browseInfo():
-    return render_template("browseInfo.html")
+    #return render_template('index.html')
+    conn = pymysql.connect(host='icrpmysql.mysql.database.chinacloudapi.cn', user='icrpmysql@icrpmysql',
+                           password='IC123rp456', db='ICRP', charset='utf8')
+    cur = conn.cursor()
+    sql = "select * from disease_infomation"
+    cur.execute(sql)
+    u = cur.fetchall()
+    conn.close()
+    return render_template('browseInfo.html',u=u)
 
+@app.route('/detail_info/<a>')
+def detail_info(a):
+    #conn = pymysql.connect(host='127.0.0.1', user='root', password='root', db='ICRP', charset='utf8')
+    conn = pymysql.connect(host='icrpmysql.mysql.database.chinacloudapi.cn', user='icrpmysql@icrpmysql', password='IC123rp456', db='ICRP', charset='utf8')
+    cur = conn.cursor()
+    a=str(a)
+    print(a)
+    sql = "select * from disease_infomation where 病名='%s'" % a
+    #print(sql)
+    cur.execute(sql)
+    u = cur.fetchall()
+    print(u[0][1])
+    conn.close()
+    return render_template('detail_info.html',u=u)
 
 if __name__ == '__main__':
     app.run()
+
+
